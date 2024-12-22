c1e15ad033ea0511528f04b4d1756114a8ae57bf
damianrein <damianrein464@gmail.com>
2024-12-21 23:19:19 -0300

    -

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private url = 'https://fakestoreapi.com/';
+  private url = 'https://dummyjson.com/products';
 
   constructor(private http:HttpClient) { }
 
   fetchAll(){
     return this.http.get<any>(this.url);
+  }
+
+  fetchProducts(): Observable<Product[]> {
+    return this.http.get(this.url).pipe(
+      map((response: any) => response.products.map((product: any) => ({
+        id: product.id,
+        name: product.title,
+        price: product.price,
+        description: product.description,
+        photos: product.images.map((image: string) => ({ id: 1, url: image }))
+      } as Product)))
+    );
   }
 }
